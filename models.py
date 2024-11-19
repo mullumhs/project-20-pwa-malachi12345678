40af267a7ed455a28b55236b18556c0c9b30ef11
malachi12345678 <140124190+malachi12345678@users.noreply.github.com>
2024-11-20 10:41:06 +1100

    added database model and setup flask and venv

--- models.py
+++ models.py
@@ -3,4 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Define your database model here
-# Example: class Item(db.Model):
+class Tree(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    coomon_name = db.Column(db.integer, primary_key=True)
+    scientific_name = db.Column(db.integer, primary_key=True)
+    genus = db.Column(db.integer, primary_key=True)
+    species = db.Column(db.integer, primary_key=True)
+    habitat_conditions = db.Column(db.integer, primary_key=True)
+    latitide_range  = db.Column(db.integer, primary_key=True)
+    population_level = db.Column(db.integer, primary_key=True)
+    max_age = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.integer, primary_key=True)
+    
+
+
