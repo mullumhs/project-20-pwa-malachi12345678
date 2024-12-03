12137d4390dab361fdd76a3e072dd720be0cf95b
malachi12345678 <140124190+malachi12345678@users.noreply.github.com>
2024-12-04 10:48:31 +1100

    working on form modal

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash
-from models import db # Also import your database model here
+from models import db, Tree # Also import your database model here
 
 # Define your routes inside the 'init_routes' function
 # Feel free to rename the routes and functions as you see fit
@@ -18,6 +18,18 @@ def init_routes(app):
 
     @app.route('/add', methods=['POST'])
     def create_item():
+        tree = Tree(
+            common_name = request.form("common_name"),
+            genus = request.form("genus"),
+            species = request.form("species"),
+            scientific_name = request.form("scientific_name"),
+            habitat_biome = request.form("habitat_biome"),
+            lifespan = request.form("lifespan"),
+            height = request.form("height"),
+            image = request.form("image"),
+            additional_info = request.form("additional_info")
+        )
+
         # This route should handle adding a new item to the database.
         return render_template('index.html', message='Item added successfully')
 
